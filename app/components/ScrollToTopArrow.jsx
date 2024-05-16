d68a6cd6316d334c431438121cae4a5b8e049482
tabishkhan03 <tabish25u@gmail.com>
2024-05-16 18:22:42 +0530

    updated scroll to top component 2nd time

--- app/components/ScrollToTopArrow.jsx
+++ app/components/ScrollToTopArrow.jsx
@@ -1,25 +1,26 @@
 "use client"
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ScrollToTopArrow = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return null;
-  }
   const [isVisible, setIsVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    // Add scroll event listener when component mounts
+    window.addEventListener("scroll", toggleVisibility);
+
+    // Clean up the event listener when component unmounts
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []); // Empty dependency array ensures useEffect runs only on mount and unmount
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -28,30 +29,30 @@ const ScrollToTopArrow = () => {
     });
   };
 
-  window.addEventListener("scroll", toggleVisibility);
-
   return (
     <div>
       {isVisible && (
-        <div className="fixed bottom-4 right-4"
-         onClick={scrollToTop}>
-        <button className="p-3 rounded-full bg-zinc-800 text-white dark:bg-white dark:text-zinc-800 shadow-lg focus:outline-none hover:bg-zinc-700 dark:hover:bg-zinc-300">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
+        <div className="fixed bottom-4 right-4">
+          <button
+            className="p-3 rounded-full bg-zinc-800 text-white shadow-lg focus:outline-none hover:bg-zinc-700"
+            onClick={scrollToTop}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M5 15l7-7 7 7"
-            />
-          </svg>
-        </button>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5 15l7-7 7 7"
+              />
+            </svg>
+          </button>
+        </div>
       )}
     </div>
   );
