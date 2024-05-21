adad932b3c6e8bc2facd2ac0632761dc64abc5cc
tabishkhan03 <tabish25u@gmail.com>
2024-05-21 22:16:22 +0530

    css update

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,19 +5,20 @@ import Autoplay from "embla-carousel-autoplay";
 
 export default function HeroSection() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+  // const [emblaRef] = useEmblaCarousel({ loop: false });
 
   return (
     <>
       <div className="embla" ref={emblaRef}>
         <div className="embla__container">
           <div className="embla__slide">
-            <img src="/pexels-fotoaibe-1668860.jpg" alt="" />
+            <img src="/pexels-fotoaibe-1668860.jpg" alt="Kitchen" />
           </div>
           <div className="embla__slide">
-            <img src="/pexels-fotoaibe-1571460.jpg" alt="" />
+            <img src="/pexels-fotoaibe-1571460.jpg" alt="Kitchen" />
           </div>
           <div className="embla__slide">
-            <img src="/pexels-fotoaibe-1643384.jpg" alt="" />
+            <img src="/pexels-fotoaibe-1643384.jpg" alt="Kitchen" />
           </div>
         </div>
       </div>
