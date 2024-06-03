2e7647637a27d43cabfef5274f6f9957820c67ff
tabishkhan03 <tabish25u@gmail.com>
2024-06-03 16:07:46 +0530

    added analytic component

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import Footer from "../app/components/Footer";
 import Header from "../app/components/Header";
 import { Inter } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,10 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <SpeedInsights/>
+      {/* <SpeedInsights/> */}
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
